81f6f22b9e8c36f6d86ab03f0cf84e45fa3de935
imvee <vind.veeru@gmail.com>
2021-09-03 20:40:04 +0530

    :zap: Added Fonts

--- login-system/src/components/Header/Header.jsx
+++ login-system/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './header.css'
 
 function Header() {
     return (
         <div className='header'>
-            <h1>Elective Management System</h1>
+            <h2>Elective Management System</h2>
             <p>Log Out</p>
         </div>
     )
