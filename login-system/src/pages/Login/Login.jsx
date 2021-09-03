a9bccf1fe2a5260cc2ba7c52073ba19c6d475878
imvee <vind.veeru@gmail.com>
2021-09-04 01:18:23 +0530

    :zap: User Context

--- login-system/src/pages/Login/Login.jsx
+++ login-system/src/pages/Login/Login.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import './login.css'
 
 function Login() {
+
+    const[username,setUsername]=useState('')
+    const[password,setPassword]=useState('')
+
+    const handleChange=()=>{
+
+    }
+
     return (
         <div className='login-page'>
             <div className='login-img'>
@@ -12,11 +20,11 @@ function Login() {
                 <form autocomplete='off'>
                     <div className='input-group'>
                         <label className='input-label' for='username'>Username</label>
-                        <input id='username' type='text' placeholder='CB.EN.U4EEE19101' className='input-box'></input>
+                        <input id='username' type='text' placeholder='CB.EN.U4EEE19101' value={username} onChange={handleChange} className='input-box' autofocus></input>
                     </div>
                     <div className='input-group'>
                         <label className='input-label' for='password'>Password</label>
-                        <input id='password' type='password' placeholder='•••••••••••••••' className='input-box'></input>
+                        <input id='password' type='password' placeholder='•••••••••••••••' value={password} className='input-box'></input>
                     </div>
                     <button className='login-btn'>Log In</button>
                 </form>
