de22f514477ff542c945d953f252023a8f3b6bfd
imvee <vind.veeru@gmail.com>
2021-09-03 20:32:18 +0530

    :zap: Cleaned Files

--- login-system/src/App.js
+++ login-system/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Login System</h1>
-      </header>
+      <Home/>
     </div>
   );
 }
