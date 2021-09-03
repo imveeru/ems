50a936f067a731b54cf6a61a80f9ba97f7de9b5e
imvee <vind.veeru@gmail.com>
2021-09-03 23:04:09 +0530

    :zap: Login Page

--- login-system/src/pages/Login/Login.jsx
+++ login-system/src/pages/Login/Login.jsx
@@ -7,14 +7,13 @@ function Login() {
             <div className='login-img'>
                 <img src='https://i.imgur.com/9pzQyj2.jpg' alt='Log in'/>
             </div>
-            <div className='login-form'>
+            <div className='login-form-box'>
                 <h1 className='title'>Elective Management System</h1>
                 <form>
-                    
-                    <p className='input-label'>Username</p>
-                    <input id='username' type='text' className='input-box'></input>
-                    <p className='input-label'>Password</p>
-                    <input id='password' type='password' className='input-box'></input>
+                        <p className='input-label'>Username</p>
+                        <input id='username' type='text' className='input-box'></input>
+                        <p className='input-label'>Password</p>
+                        <input id='password' type='password' className='input-box'></input>
                 </form>
             </div>
         </div>
