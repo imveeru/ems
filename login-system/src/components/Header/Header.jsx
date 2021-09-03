692f5f03e4883bccd06ee435688d1f3bb323f3bb
imvee <vind.veeru@gmail.com>
2021-09-03 20:28:48 +0530

    :zap: Cleaned Files

---
+++ login-system/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>Elective Management System</h1>
+            <p>Log Out</p>
+        </div>
+    )
+}
+
+export default Header
