d3f4bea04f353db0ffcf8bb1424c5755d6de14d2
imvee <vind.veeru@gmail.com>
2021-09-03 20:59:30 +0530

    :zap: Added Fonts

--- login-system/src/components/Header/Header.jsx
+++ login-system/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import './header.css'
 function Header() {
     return (
         <div className='header'>
-            <h2>Elective Management System</h2>
-            <p>Log Out</p>
+            <p>Elective Management System</p>
+            <button className='logout-btn'>Log Out</button>
         </div>
     )
 }
