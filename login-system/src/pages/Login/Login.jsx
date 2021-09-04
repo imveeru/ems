9de9b647c7f136f2c2222255cc4d1aaaee165202
imvee <vind.veeru@gmail.com>
2021-09-04 16:14:48 +0530

    :sparkles: Login Form

--- login-system/src/pages/Login/Login.jsx
+++ login-system/src/pages/Login/Login.jsx
@@ -17,14 +17,14 @@ function Login() {
             </div>
             <div className='login-form-box'>
                 <h1 className='title'>Elective Management System<span className='dot'>.</span></h1>
-                <form autocomplete='off' onSubmit={handleSubmit}>
+                <form autoComplete='off' onSubmit={handleSubmit}>
                     <div className='input-group'>
-                        <label className='input-label' for='username'>Username</label>
-                        <input id='username' type='text' placeholder='CB.EN.U4EEE19101' value={username} onChange={handleChange} className='input-box' autofocus></input>
+                        <label className='input-label' htmlFor='username'>Username</label>
+                        <input id='username' type='text' placeholder='CB.EN.U4EEE19101' className='input-box' autoFocus></input>
                     </div>
                     <div className='input-group'>
-                        <label className='input-label' for='password'>Password</label>
-                        <input id='password' type='password' placeholder='•••••••••••••••' value={password} className='input-box'></input>
+                        <label className='input-label' htmlFor='password'>Password</label>
+                        <input id='password' type='password' placeholder='•••••••••••••••' className='input-box'></input>
                     </div>
                     <button className='login-btn' type='submit'>Log In</button>
                 </form>
