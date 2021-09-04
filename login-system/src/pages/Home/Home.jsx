8c81573cb37dcea78bbfd8123acd066ef52cc2ae
imvee <vind.veeru@gmail.com>
2021-09-04 17:05:56 +0530

    :sparkles: Home page

--- login-system/src/pages/Home/Home.jsx
+++ login-system/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
-
+import {useAuth} from '../../context/AuthContext'
 
 function Home() {
+
+    const {currentUser}=useAuth()
+
     return (
         <div>
             <Header />
+            <h1>Welcome {currentUser.email}</h1>
         </div>
     )
 }
