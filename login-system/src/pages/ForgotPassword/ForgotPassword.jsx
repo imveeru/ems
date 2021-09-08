efe3f7e27f0cb0b99c2d56135dfcd35368600601
imvee <vind.veeru@gmail.com>
2021-09-08 21:08:37 +0530

    :zap: Added forgot password page.

---
+++ login-system/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useAuth} from '../../context/AuthContext'
+
+function ForgotPassword() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ForgotPassword
