c1178e720fc1e3b0f2c9eb2f426e2c5015c2cfa7
imvee <vind.veeru@gmail.com>
2021-09-08 21:49:18 +0530

    :zap: Added forgot password page.

--- login-system/src/pages/ForgotPassword/ForgotPassword.jsx
+++ login-system/src/pages/ForgotPassword/ForgotPassword.jsx
@@ -37,19 +37,15 @@ function ForgotPassword() {
                 <img src='https://i.imgur.com/9pzQyj2.jpg' alt='Log in'/>
             </div>
             <div className='login-form-box'>
-                <div className='title'><h3>Forgot Password?</h3></div>
+                <div className='title'><h3>Forgot Password<span className='dot'>?</span></h3></div>
                 <form autoComplete='off' onSubmit={handleSubmit}>
                     <div className='input-group'>
                         <label className='input-label' htmlFor='username'>Username</label>
                         <input id='username' type='text' placeholder='CB.EN.U4EEE19101' ref={usernameRef} className='input-box' autoFocus required></input>
                     </div>
-                    <div className='input-group'>
-                        <label className='input-label' htmlFor='password'>Password</label>
-                        <input id='password' type='password' placeholder='•••••••••••••••' minLength='8' ref={passwordRef} className='input-box' required></input>
-                    </div>
                     <div className='input-btn-group'>
                         <button className='login-btn' type='submit'>Log In</button>
-                        <Link to='/forgot-password' style={{'text-decoration':'none'}}>Forgot Password?</Link>
+                        <Link to='/login' style={{'text-decoration':'none'}}>Login</Link>
                     </div>
                 </form>
             </div>
