f642619f5ebfc0c380fb5ed2404afffe3b1d8eb9
imvee <vind.veeru@gmail.com>
2021-10-20 01:24:00 +0530

    :zap: Electives component

--- login-system/src/components/ElectiveList/ElectiveList.jsx
+++ login-system/src/components/ElectiveList/ElectiveList.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 function ElectiveList({electives}) {
     return (
-        <div>
-            {
-                electives.map((elective) =>(
-                    <p>{elective.sem}-{elective.elective_1}-{elective.elective_2}-{elective.elective_3}</p>
-                ))
-            }
+        <div className="previous-electives-container">
+            <h2>Electives of Previous Semesters</h2>
+                {electives.map((elective) =>(
+                        <div key={elective.sem} className="previous-elective">
+                            <div className="sem-name">Semester - {elective.sem}</div>
+                            <div className="sem-list">
+                                <p>Elective 1 - {elective.elective_1===null?"No Elective":elective.elective_1}</p>
+                                <p>Elective 2 - {elective.elective_2===null?"No Elective":elective.elective_2}</p>
+                                <p>Elective 3 - {elective.elective_3===null?"No Elective":elective.elective_3}</p>
+                            </div>
+                        </div> 
+                ))}
         </div>
     )
 }
