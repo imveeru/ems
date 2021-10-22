6dbfa8461bba89e6db555ddb6a5960ad2ce20117
imvee <vind.veeru@gmail.com>
2021-10-23 01:20:01 +0530

    :zap: Fetched course details

--- login-system/src/pages/Course/Course.jsx
+++ login-system/src/pages/Course/Course.jsx
@@ -1,11 +1,28 @@
 import React,{useState,useEffect} from 'react'
 import Header from '../../components/Header/Header'
+import {db} from '../../firebase'
 
-function Course() {
+function Course(props) {
+    console.log(props.match.params.courseId);
+    const courseDbRef=db.collection('courses')
+
+    const[course,setCourse]=useState({})
+
+    const fetchUserData=async()=>{
+        const res=courseDbRef.doc(props.match.params.courseId)
+        await res.onSnapshot((doc)=>{
+            setCourse(doc.data())
+        })
+    }
+
+    useEffect(()=>{
+        fetchUserData()
+    })
+
     return (
         <div>
             <Header/>
-            
+            {course?'Irukku':'illa'}
         </div>
     )
 }
