4b41044265d16aa092f457b6518e36d3c5187b59
imvee <vind.veeru@gmail.com>
2021-10-29 01:30:38 +0530

    :zap: Added add user basic

---
+++ login-system/src/components/AdminForms/AddFaculty.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddFaculty() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AddFaculty
--- login-system/src/components/AdminForms/AddUser.jsx
+++ login-system/src/components/AdminForms/AddUser.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {useForm} from 'react-hook-form'
+import AddStudent from './AddStudent'
+import AddFaculty from './AddFaculty'
 
 function AddUser() {
+
+    const{ register, handleSubmit,watch } = useForm();
+
+    const[formData,setFormData] = useState()
+
+    const onSubmit = (data)=>setFormData(data)
+
+    const role=watch("role");
+
     return (
-        <div>
-            Add User
+        <div className="add-form">
+            <h2>Add User</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <select {...register("role")}>
+                <option value="">Select user role...</option>
+                <option value="student">Student</option>
+                <option value="faculty">Faculty</option>
+            </select>
+            {role==='student'?<AddStudent/>:role==='faculty'?<AddFaculty/>:null}
+            </form>
         </div>
     )
 }
