42605052b2e43e3e663953e122be4d9b2caae7e5
imvee <vind.veeru@gmail.com>
2021-10-28 23:47:41 +0530

    :zap: Adding Admin functions

--- login-system/src/pages/Admin/Admin.jsx
+++ login-system/src/pages/Admin/Admin.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import {GrNotes,GrUserAdd,GrCompliance} from 'react-icons/gr'
+
+import './Admin.css'
 
 function Admin({userData}) {
     return (
             <div className="home-container">
                 <h1>Welcome {userData.name}!</h1>
-                <div className="user-details">
+                <div className="admin-functions">
+                    <button className="btn-add">
+                        <GrUserAdd/>
+                        Add User
+                    </button>
+
+                    <button className="btn-add">
+                        <GrNotes/>
+                        Add Course
+                    </button>
+                    
+                    <button className="btn-add">
+                        <GrCompliance/>
+                        Assign Course
+                    </button>
                 </div>
             </div>
     )
