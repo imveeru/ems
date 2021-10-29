159389ea97254115cb805bbb7ff74602968267c1
imvee <vind.veeru@gmail.com>
2021-10-29 17:40:21 +0530

    :sparkles: Improved course page

--- login-system/src/components/AdminForms/AddUser.jsx
+++ login-system/src/components/AdminForms/AddUser.jsx
@@ -2,14 +2,29 @@ import React,{useState} from 'react'
 import {useForm} from 'react-hook-form'
 import AddStudent from './AddStudent'
 import AddFaculty from './AddFaculty'
+import {getAuth} from "firebase/auth"
 
 function AddUser() {
 
     const{ register, handleSubmit,watch } = useForm();
 
-    const[formData,setFormData] = useState()
+    const[formData,setFormData] = useState({})
 
-    const onSubmit = (data)=>setFormData(data)
+    
+
+    const onSubmit = (data)=>{
+        setFormData(data)
+        // getAuth().createUser({
+        //     email:data.username+"@cb.students.amrita.edu",
+        //     password:data.password
+        // })
+        // .then((response)=> {
+        //     console.log("User Created Successfully!",response.uid);
+        // })
+        // .catch((error)=> {
+        //     console.error("Error Creating new user",error)
+        // });
+    }
 
     const role=watch("role");
 
@@ -17,13 +32,17 @@ function AddUser() {
         <div className="add-form">
             <h2>Add User</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("usernameRef")} placeholder="Enter the username" type="text"></input>
+            <input {...register("password")} placeholder="Enter the password" type="text"></input>
             <select {...register("role")}>
                 <option value="">Select user role...</option>
                 <option value="student">Student</option>
                 <option value="faculty">Faculty</option>
             </select>
-            {role==='student'?<AddStudent/>:role==='faculty'?<AddFaculty/>:null}
+            <button type="submit">Add user</button>
             </form>
+            <p>{JSON.stringify(formData)}</p>
+            {role==='student'?<AddStudent/>:role==='faculty'?<AddFaculty/>:null}
         </div>
     )
 }
