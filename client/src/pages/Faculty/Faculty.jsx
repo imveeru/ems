6e9bbcf87b57d231ef1d90c3e00673c1ef62ad6c
imvee <vind.veeru@gmail.com>
2021-11-02 22:46:05 +0530

    :art: Fetched elective details for faculty

--- client/src/pages/Faculty/Faculty.jsx
+++ client/src/pages/Faculty/Faculty.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {db} from '../../firebase'
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { Link } from 'react-router-dom';
 
 function Faculty({userData}) {
+
+    const[assignedCourses,setAssignedCourses]=useState([])
+
+    const q = query(collection(db, "electives"), where("faculty", "==", userData.name));
+
+    const fetchUserData=async()=>{
+
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const assignedCourse = [];
+            querySnapshot.forEach((doc) => {
+                assignedCourse.push(doc.data());
+            });
+
+            //console.log(assignedCourse);
+            setAssignedCourses(assignedCourse);
+        });
+    }
+
+    useEffect(()=>{
+        fetchUserData();
+    },[])
+
     return (
             <div className="home-container">
                 <h1>Welcome {userData.name}!</h1>
@@ -9,7 +34,15 @@ function Faculty({userData}) {
                     <p>{userData.grade}</p>
                 </div>
                 <div className="course-assigned">
-
+                    {assignedCourses.map((assignedCourse)=>(
+                        <div className="assigned-course">
+                        <Link to={"/course/"+assignedCourse.courseCode} style={{'textDecoration':'none','color':'black'}}><p className='elective'>{assignedCourse.courseCode}</p></Link>
+                        <p>{assignedCourse.batch} Batch - {assignedCourse.dept} Dept. - Semester {assignedCourse.sem}</p>
+                        <p>{assignedCourse.faculty}</p>
+                        <p>Max Sots : {assignedCourse.maxLimit}</p>
+                        <p>Remaining Slots : {assignedCourse.maxLimit-assignedCourse.studentList.length}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
     )
