de85c42cea68cbb049deb75b33ae3de757cfa318
imvee <vind.veeru@gmail.com>
2021-11-08 20:29:25 +0530

    :sparkles: Fecth assigned courses

--- client/src/pages/Faculty/Faculty.jsx
+++ client/src/pages/Faculty/Faculty.jsx
@@ -2,6 +2,10 @@ import React,{useState,useEffect} from 'react'
 import {db} from '../../firebase'
 import { collection, query, where, onSnapshot } from "firebase/firestore";
 import { Link } from 'react-router-dom';
+import './Faculty.css'
+import {CircularProgressbar,buildStyles} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 function Faculty({userData}) {
 
@@ -39,9 +43,23 @@ function Faculty({userData}) {
                         <Link to={"/course/"+assignedCourse.courseCode} style={{'textDecoration':'none','color':'black'}}><p className='elective'>{assignedCourse.courseCode}</p></Link>
                         <p>{assignedCourse.batch} Batch - {assignedCourse.dept} Dept. - Semester {assignedCourse.sem}</p>
                         <p>{assignedCourse.faculty}</p>
+                        <CircularProgressbar
+                            value={parseInt(assignedCourse.studentList.length)}
+                            text={`${parseInt(assignedCourse.maxLimit)-parseInt(assignedCourse.studentList.length)}/${assignedCourse.maxLimit}`}
+                            styles={buildStyles({
+                                textSize: "14px",
+                                pathColor: 'rgba(46,106,218)',
+                                textColor: 'rgba(46,106,218)',
+                                trailColor: 'rgba(46,106,218,0.1)'
+                              })}
+                            strokeWidth={15}
+                            minValue={0}
+                            maxValue={parseInt(assignedCourse.maxLimit)}
+                            className='progress-bar'
+                        ></CircularProgressbar>
                         <p>Max Sots : {assignedCourse.maxLimit}</p>
                         <p>Remaining Slots : {assignedCourse.maxLimit-assignedCourse.studentList.length}</p>
-                        <table>
+                        <table className="studentList">
                             <tr>
                                 <th>Student Roll Number</th>
                             </tr>
