3428c6e7eed4cb82bbec6964996d932854de6f4c
imvee <vind.veeru@gmail.com>
2021-11-08 21:50:40 +0530

    :sparkles: Fecth assigned courses

---
+++ client/src/components/Assigned Course/AssignedCourse.jsx
@@ -0,0 +1,60 @@
+import React,{useState} from 'react'
+import {CircularProgressbar,buildStyles} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { Link } from 'react-router-dom';
+import {MdExpandMore,MdExpandLess} from 'react-icons/md'
+
+function AssignedCourse({assignedCourses}) {
+
+    const [expanded, setExpanded] = useState(false)
+
+    return (
+        <div className="course-assigned">
+                    {assignedCourses.map((assignedCourse)=>(
+                        <div className="assigned-course">
+                        
+                        <CircularProgressbar
+                            value={parseInt(assignedCourse.studentList.length)}
+                            text={`${parseInt(assignedCourse.maxLimit)-parseInt(assignedCourse.studentList.length)}/${assignedCourse.maxLimit}`}
+                            styles={buildStyles({
+                                textSize: "14px",
+                                pathColor: 'rgba(46,106,218)',
+                                textColor: 'rgba(46,106,218)',
+                                trailColor: 'rgba(46,106,218,0.1)'
+                              })}
+                            strokeWidth={15}
+                            minValue={0}
+                            maxValue={parseInt(assignedCourse.maxLimit)}
+                            className='progress-bar'
+                        ></CircularProgressbar>
+
+                        <div className="assigned-course-details">
+                            <Link className="assigned-course-title" to={"/course/"+assignedCourse.courseCode} style={{'textDecoration':'none','color':'black'}}><p className='elective'>{assignedCourse.courseCode}</p></Link>
+                            <p>{assignedCourse.batch} Batch - {assignedCourse.dept} Dept. - Semester {assignedCourse.sem}</p>
+                        </div>
+                        
+                        <button className="expand-btn">
+                              {expanded?<MdExpandLess/>:<MdExpandMore/>}
+                        </button>
+                        {/* <p>{assignedCourse.faculty}</p> */}
+                        
+                        {/* <p>Max Sots : {assignedCourse.maxLimit}</p>
+                        <p>Remaining Slots : {assignedCourse.maxLimit-assignedCourse.studentList.length}</p> */}
+
+                            {/* <table className="studentList">
+                                <tr>
+                                    <th>Student Roll Number</th>
+                                </tr>
+                                {assignedCourse.studentList.map((student)=>(
+                                    <tr key={student}>
+                                        <td>{student}</td>
+                                    </tr>
+                                ))}
+                            </table> */}
+                        </div>
+                    ))}
+                </div>
+    )
+}
+
+export default AssignedCourse
--- client/src/pages/Faculty/Faculty.jsx
+++ client/src/pages/Faculty/Faculty.jsx
@@ -1,11 +1,8 @@
 import React,{useState,useEffect} from 'react'
 import {db} from '../../firebase'
 import { collection, query, where, onSnapshot } from "firebase/firestore";
-import { Link } from 'react-router-dom';
 import './Faculty.css'
-import {CircularProgressbar,buildStyles} from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
+import AssignedCourse from '../../components/Assigned Course/AssignedCourse';
 
 function Faculty({userData}) {
 
@@ -37,46 +34,7 @@ function Faculty({userData}) {
                     <p>{userData.dept}</p>
                     <p>{userData.grade}</p>
                 </div>
-                <div className="course-assigned">
-                    {assignedCourses.map((assignedCourse)=>(
-                        <div className="assigned-course">
-                        
-                        <CircularProgressbar
-                            value={parseInt(assignedCourse.studentList.length)}
-                            text={`${parseInt(assignedCourse.maxLimit)-parseInt(assignedCourse.studentList.length)}/${assignedCourse.maxLimit}`}
-                            styles={buildStyles({
-                                textSize: "14px",
-                                pathColor: 'rgba(46,106,218)',
-                                textColor: 'rgba(46,106,218)',
-                                trailColor: 'rgba(46,106,218,0.1)'
-                              })}
-                            strokeWidth={15}
-                            minValue={0}
-                            maxValue={parseInt(assignedCourse.maxLimit)}
-                            className='progress-bar'
-                        ></CircularProgressbar>
-
-                        <Link to={"/course/"+assignedCourse.courseCode} style={{'textDecoration':'none','color':'black'}}><p className='elective'>{assignedCourse.courseCode}</p></Link>
-                        <p>{assignedCourse.batch} Batch - {assignedCourse.dept} Dept. - Semester {assignedCourse.sem}</p>
-                        
-                        {/* <p>{assignedCourse.faculty}</p> */}
-                        
-                        {/* <p>Max Sots : {assignedCourse.maxLimit}</p>
-                        <p>Remaining Slots : {assignedCourse.maxLimit-assignedCourse.studentList.length}</p> */}
-
-                        <table className="studentList">
-                            <tr>
-                                <th>Student Roll Number</th>
-                            </tr>
-                            {assignedCourse.studentList.map((student)=>(
-                                <tr>
-                                    <td>{student}</td>
-                                </tr>
-                            ))}
-                        </table>
-                        </div>
-                    ))}
-                </div>
+                <AssignedCourse assignedCourses={assignedCourses}></AssignedCourse>
             </div>
     )
 }
