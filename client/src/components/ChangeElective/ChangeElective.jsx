a4f07019748984cd7e9f38a0a705c4716a546aee
imvee <vind.veeru@gmail.com>
2021-11-14 00:42:29 +0530

    :zap: Added change elective component

---
+++ client/src/components/ChangeElective/ChangeElective.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ChangeElective.css'
+
+function ChangeElective() {
+    return (
+        <div className='change-elective-container'>
+            <h2>Change Elective</h2>
+        </div>
+    )
+}
+
+export default ChangeElective
