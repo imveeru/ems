67071abcf3dfd928c95805fd30bbd5f43f1443bc
imvee <vind.veeru@gmail.com>
2021-11-14 01:22:06 +0530

    :zap: Added change elective component

--- client/src/components/ChangeElective/ChangeElective.jsx
+++ client/src/components/ChangeElective/ChangeElective.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { doc, getDoc } from "firebase/firestore";
 import './ChangeElective.css'
+import {db} from '../../firebase'
+import { useEffect } from 'react';
 
-function ChangeElective({alreadyEnrolledCourses}) {
+function ChangeElective({alreadyEnrolledCourses,userData}) {
+
+    const[courseData,setCourseData]=useState([])
+
+    const fetchCourseData=()=>{
+        var courseDataList=[]
+        alreadyEnrolledCourses.forEach(async (course) =>{
+            const docRef = doc(db, "electives", course+"_"+userData.yearJoined+"_"+userData.currentSem+"_"+userData.branch);
+            const docSnap = await getDoc(docRef);
+            if(docSnap){
+                courseDataList.push(docSnap.data());
+            }
+        })
+        setCourseData(courseDataList)
+    }
+    
+    useEffect(() => {
+        fetchCourseData()
+    },[])
+
     return (
         <div className='change-elective-container'>
             <h2>Change Elective<span className='title-tooltip'> Select the course you wish to change and send request to the respective handling faculty.</span></h2>
@@ -12,6 +34,7 @@ function ChangeElective({alreadyEnrolledCourses}) {
                 ))}
             </div>
             </p>
+            <p>{JSON.stringify(userData)}</p>
         </div>
     )
 }
