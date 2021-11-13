c126b5ed5a5c2c7b87ec96dd7ab1cc2c0e0212f7
imvee <vind.veeru@gmail.com>
2021-11-14 01:00:17 +0530

    :zap: Added change elective component

--- client/src/components/ChangeElective/ChangeElective.jsx
+++ client/src/components/ChangeElective/ChangeElective.jsx
@@ -5,10 +5,10 @@ function ChangeElective({alreadyEnrolledCourses}) {
     return (
         <div className='change-elective-container'>
             <h2>Change Elective<span className='title-tooltip'> Select the course you wish to change and send request to the respective handling faculty.</span></h2>
-            <p>Enrolled Courses : 
+            <p>Enrolled Courses
             <div className='user-details'>
                 {alreadyEnrolledCourses.map((course, index) =>(
-                    <p>{course}</p>
+                    <p key={index}>{course}</p>
                 ))}
             </div>
             </p>
