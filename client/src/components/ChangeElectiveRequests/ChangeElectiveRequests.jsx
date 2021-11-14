52a714685a41dd3c5a6811777ff6c67462cdd6e6
imvee <vind.veeru@gmail.com>
2021-11-15 01:13:44 +0530

    :zap: Fetched change requests

--- client/src/components/ChangeElectiveRequests/ChangeElectiveRequests.jsx
+++ client/src/components/ChangeElectiveRequests/ChangeElectiveRequests.jsx
@@ -4,7 +4,8 @@ function ChangeElectiveRequests({request}) {
     return (
         <div className="request">
             <p><strong>{request.sender}</strong> has requested you to change his/her elective from <strong>{request.alreadyEnrolled.split("_")[0]}</strong> to <strong>{request.newElective.split("_")[0]}</strong></p>
-        
+            <button className="add-btn">Approve</button>
+            <button className="add-btn">Approve</button>
         </div>
     )
 }
