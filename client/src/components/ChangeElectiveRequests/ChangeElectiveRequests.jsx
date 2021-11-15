6dd292fc9f40d2d2c02acb201aec35b0f3301881
imvee <vind.veeru@gmail.com>
2021-11-15 14:08:21 +0530

    :zap: Handling request approval

--- client/src/components/ChangeElectiveRequests/ChangeElectiveRequests.jsx
+++ client/src/components/ChangeElectiveRequests/ChangeElectiveRequests.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import {useForm} from 'react-hook-form'
-import { collection, query, where, onSnapshot,updateDoc, arrayUnion,doc } from "firebase/firestore";
+import { collection, onSnapshot,updateDoc,doc, arrayRemove, arrayUnion } from "firebase/firestore";
 import {db} from '../../firebase'
 
 function ChangeElectiveRequests({request}) {
@@ -12,9 +12,21 @@ function ChangeElectiveRequests({request}) {
     const handleApprove=async (data)=>{
         setFormData(data)
         let reqRef=doc(db,"changeRequests",data.requestId)
-            await updateDoc(reqRef, {
+        await updateDoc(reqRef, {
             decisionMade:"yes",
             isApproved:"yes"
+        });
+
+        // remove from old course
+        let removeRef=doc(db,"electives",data.alreadyEnrolled)
+        await updateDoc(removeRef, {
+            studentList:arrayRemove(data.sender)
+        });
+
+        //add in new course
+        let addRef=doc(db,"electives",data.newElective)
+        await updateDoc(addRef, {
+            studentList:arrayUnion(data.sender)
         });
 
     }
@@ -31,6 +43,9 @@ function ChangeElectiveRequests({request}) {
         <div className="request">
             <p><strong>{request.sender}</strong> has requested you to change his/her elective from <strong>{request.alreadyEnrolled.split("_")[0]}</strong> to <strong>{request.newElective.split("_")[0]}</strong></p>
             <form>
+            <input type="hidden" {...register("alreadyEnrolled")} value={request.alreadyEnrolled} ></input>
+            <input type="hidden" {...register("newElective")} value={request.newElective} ></input>
+            <input type="hidden" {...register("sender")} value={request.sender} ></input>
             <input type="hidden" {...register("requestId")} value={request.alreadyEnrolled+"_to_"+request.newElective+"_from_"+request.sender} ></input>
             <button className="add-btn" onClick={handleSubmit(handleApprove)}>Approve</button>
             <button className="reject-btn" onClick={handleSubmit(handleReject)}>Reject</button>
